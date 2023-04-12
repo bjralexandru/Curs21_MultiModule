45940f39a9a0d67c5aafaeb5a0ae7dc565a81a44
Mihai Bujor <bjalexandru@gmail.com>
2023-04-12 21:45:36 +0300

    Postman request
    Signed-off-by: Mihai Bujor <bjalexandru@gmail.com>

--- Authy/src/main/java/bjr/spring/login/AuthyApp.java
+++ Authy/src/main/java/bjr/spring/login/AuthyApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLoginExampleApplication {
+public class AuthyApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLoginExampleApplication.class, args);
+		SpringApplication.run(AuthyApp.class, args);
 	}
 
 }
